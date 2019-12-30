118f5d4c0c0942d67823d9799669325ad8956f0a
saadshamim95 <saadshamim95@gmail.com>
2019-12-30 17:38:48 +0530

    Stack (Balanced Paranthesis) implemented!

--- Data Structure/Program.cs
+++ Data Structure/Program.cs
@@ -4,9 +4,30 @@ namespace Data_Structure
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stacks<char> stacks = new Stacks<char>();
+            //string str = "(5+6)*(7+8)/(4+3)(5+6)*(7+8)/(4+3)";
+            string str = "((5+(6-3)*((3/7)+3))/(11+13*((7/15)*(3+(15*5)))))";
+            char[] charArray = str.ToCharArray();
+            for (int i = 0; i < charArray.Length; i++) {
+                if (charArray[i] == '(')
+                    stacks.push('(');
+                else if (charArray[i] == ')')
+                    stacks.pop();
+                Console.Write("Stack: ");
+                stacks.Print();
+                Console.WriteLine();
+            }
+            bool value = stacks.isEmpty();
+            if (value)
+                Console.WriteLine("Balanced Paranthesis!");
+            else
+                Console.WriteLine("Unbalanced Paranthesis!");
         }
     }
 }
---
+++ Data Structure/Stacks.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    class Stacks<T>
+    {
+        T[] array = new T[10];
+        public int top=-1;
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Stacks"/> class from being created.
+        /// </summary>
+        public Stacks() {
+            top = -1;
+        }
+
+        /// <summary>
+        /// Pushes the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void push(T item) {
+            if (top == 10) {
+                Console.WriteLine("Error! Stack Overflown...");
+                return;
+            }
+            array[++top] = item;
+        }
+
+        /// <summary>
+        /// Pops this instance.
+        /// </summary>
+        /// <returns></returns>
+        public void pop() {
+            if (top == -1) {
+                Console.WriteLine("Error! Stack has no element to pop...");
+                return;
+            } 
+            T item = array[top];
+            top--;
+            //return item;
+        }
+
+        /// <summary>
+        /// Peeks this instance.
+        /// </summary>
+        /// <returns></returns>
+        T peek() {
+            return array[top];
+        }
+
+        /// <summary>
+        /// Determines whether this instance is empty.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </returns>
+        public bool isEmpty() {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Sizes this instance.
+        /// </summary>
+        /// <returns></returns>
+        public int size() {
+            return (top + 1);
+        }
+
+        /// <summary>
+        /// Prints this instance.
+        /// </summary>
+        public void Print() {
+            for (int i = 0; i <= top; i++)
+                Console.Write(array[i] + " ");
+        }
+    }
+}

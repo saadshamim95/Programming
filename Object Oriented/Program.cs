d929802d2fa99999210c9206df438643e7067e7b
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-11 11:44:26 +0530

    [Saad] Add. adding file in Object Oriented

---
+++ Object Oriented/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Object_Oriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*InventoryManager manager = new InventoryManager();
+            manager.Test();
+
+            StockPortfolio portfolio = new StockPortfolio();
+            portfolio.Test();*/
+
+            DeckOfCards deck = new DeckOfCards();
+            deck.Initialize();
+        }
+    }
+}

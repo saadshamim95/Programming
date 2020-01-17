7ae7749b8f99074365e9bb4c5d26bbed0f8feed6
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-17 19:25:06 +0530

    [Saad] Add. Adding Design Pattern solution

---
+++ Design Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

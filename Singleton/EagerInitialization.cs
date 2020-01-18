9cfaf193e2a0281a75c5d95f99ead649c586a570
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-18 17:17:55 +0530

    [Saad] Add. Adding Singleton EagerInitialization.cs

---
+++ Singleton/EagerInitialization.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EagerInitialization.cs" company="BridgeLabz">
+//     Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+
+namespace Design_Pattern
+{
+    /// <summary>
+    /// Class for Eager Initialization of instance
+    /// </summary>
+    public class EagerInitialization
+    {
+        /// <summary>
+        /// The instance
+        /// </summary>
+        private static EagerInitialization instance = new EagerInitialization();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="EagerInitialization"/> class from being created.
+        /// </summary>
+        private EagerInitialization()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <returns>Instance of a Singleton class</returns>
+        public static EagerInitialization GetInstance()
+        {
+            return instance;
+        }
+    }
+}

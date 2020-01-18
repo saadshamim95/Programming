42e73d35932d2fa74d1b3db79ef4e43d7af9ca2c
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-18 17:48:24 +0530

    [Saad] Add. Adding StaticBlockInitilization.cs

---
+++ Singleton/StaticBlockInitialization.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StaticBlockInitialization.cs" company="BridgeLabz">
+//     Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+
+namespace Design_Pattern
+{
+    using System;
+
+    /// <summary>
+    /// Class for Static Block Initialization
+    /// </summary>
+    public class StaticBlockInitialization
+    {
+        /// <summary>
+        /// The instance
+        /// </summary>
+        private static StaticBlockInitialization instance;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="StaticBlockInitialization"/> class.
+        /// </summary>
+        static StaticBlockInitialization()
+        {
+            try
+            {
+                instance = new StaticBlockInitialization();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception occurred in creating singleton instance");
+            }
+        }
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="StaticBlockInitialization"/> class from being created.
+        /// </summary>
+        private StaticBlockInitialization()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <returns>Instance of a Singleton class</returns>
+        public static StaticBlockInitialization GetInstance()
+        {
+            return instance;
+        }
+    }
+}

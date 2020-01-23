89a56f6c68d33c2209578744084870f25e83d30c
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-23 11:41:44 +0530

    [Saad] Add. Adding Exception Handling

---
+++ ExceptionHandling/ExceptionHandling.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExceptionHandling.cs" company="BridgeLabz">
+//     Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+
+namespace Design_Pattern
+{
+    using System;
+
+    /// <summary>
+    /// Class for Exception Handling
+    /// </summary>
+    public class ExceptionHandling
+    {
+        /// <summary>
+        /// Tests this instance.
+        /// </summary>
+        public void Test()
+        {
+            int i = 100;
+            int j = 0;
+            int result = 0;
+
+            try
+            {
+                result = i / j;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by Zero Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Result: " + result);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ namespace Design_Pattern
             Console.WriteLine("10: Reflection");
             Console.WriteLine("11: LateBinding using Reflection");
             Console.WriteLine("12: Dependency Injection");
+            Console.WriteLine("13: Exception Handling");
             Console.Write("Enter you choice: ");
             int choice = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
@@ -84,6 +85,10 @@ namespace Design_Pattern
                 case 12:
                     DependencyInjection dependencyInjection = new DependencyInjection();
                     dependencyInjection.Test();
+                    break;
+                case 13:
+                    ExceptionHandling exceptionHandling = new ExceptionHandling();
+                    exceptionHandling.Test();
                     break;
                 default:
                     Console.WriteLine("Invalid Input!!!");

0b3b5b66d18c602371d918d42c685cd101bc55d7
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-01-30 13:03:13 +0530

    [Saad] Add. Adding html, css, js files

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -4,19 +4,89 @@ namespace EmployeeManagementSystem.Model
 {
     public class Employee
     {
-        [Key]
-        public int EmployeeID { get; set; }
+        private string firstName;
+        private string lastName;
+        private string userName;
+        private string pass;
+        private string email;
+        private string mobile;
+
         [Required]
-        public string FirstName { get; set; }
+        public string FirstName 
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
         [Required]
-        public string LastName { get; set; }
+        public string LastName 
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
 		[Required]
-		public string UserName {get; set; }
+		public string UserName 
+        {
+            get
+            {
+                return this.userName;
+            }
+            set
+            {
+                this.userName = value;
+            }
+        }
+
 		[Required]
-		public string Pass {get; set; }
+		public string Pass 
+        {
+            get
+            {
+                return this.pass;
+            }
+            set
+            {
+                this.pass = value;
+            }
+        }
+
         [Required]
-        public string Email { get; set; }
+        public string Email 
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
         [Required]
-        public string Mobile { get; set; }
+        public string Mobile 
+        {
+            get
+            {
+                return this.mobile;
+            }
+            set
+            {
+                this.mobile = value;
+            }
+        }
     }
 }
---
+++ wwwroot/Script.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+        $(".Login-form").hide();
+    $(".Login").css("background", "none");
+
+            $(".Login").click(function () {
+        $(".SignUp-form").hide();
+    $(".Login-form").show();
+    $(".SignUp").css("background", "none");
+    $(".Login").css("background", "#fff");
+});
+
+            $(".SignUp").click(function () {
+        $(".Login-form").hide();
+    $(".SignUp-form").show();
+    $(".Login").css("background", "none");
+    $(".SignUp").css("background", "#fff");
+});
+});

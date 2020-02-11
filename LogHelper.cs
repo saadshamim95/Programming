3c227b39bb3a15e73cd691dddeca46a35fc283e7
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-11 17:24:29 +0530

    [Saad] Refactor. Logger completed

--- LogHelper.cs
+++ LogHelper.cs
@@ -1,9 +1,24 @@
-﻿using System.Runtime.CompilerServices;
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogHelper.cs" company="BridgeLabz">
+//     Company copyright tag.
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
 
 namespace LearningLogger
 {
-    class LogHelper
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Class LogHelper
+    /// </summary>
+    public class LogHelper
     {
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <returns>returns file path</returns>
         public static log4net.ILog GetLogger([CallerFilePath]string filename = "")
         {
             return log4net.LogManager.GetLogger(filename);
--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
-﻿using log4net;
-using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="BridgeLabz">
+//     Company copyright tag.
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace LearningLogger
 {
-    class Program
+    using System;
+    using log4net;
+    
+    /// <summary>
+    /// Class for testing Logger
+    /// </summary>
+    public class Program
     {
-        private static readonly ILog log = LogHelper.GetLogger(); //LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        /// <summary>
+        /// The log
+        /// </summary>
+        private static readonly ILog Log = LogHelper.GetLogger();
 
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            log.Error("This is my error message");
+            Log.Error("This is my error message");
             Console.ReadLine();
         }
     }

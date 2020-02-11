86f035f7a450782948fe3172c87ada244795927e
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-11 14:47:53 +0530

    [Saad] Refactor. Added Console Appender in codebase

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace LearningLogger
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            log.Error("This is my error message");
+            Console.ReadLine();
+        }
+    }
+}

af4bc79a77fb6d34f2a26ffe66803c7be8af6223
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-19 16:04:20 +0530

    [Saad]. Refactor. Adding test case for mobile number

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿//-----------------------------------------------------------------------
+// <copyright file="UserRegistration.cs" company="BridgeLabz">
+//     Copyright © 2020 
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
 
 namespace XUnitTesting
 {
-    class UserRegistration
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// class for User Registration
+    /// </summary>
+    public class UserRegistration
     {
+        /// <summary>
+        /// Firsts the name.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns>It returns true if pattern matches.</returns>
         public bool FirstName(string firstName)
         {
             var pattern = "^[a-zA-Z]+$";
@@ -19,6 +31,11 @@ namespace XUnitTesting
             return false;
         }
 
+        /// <summary>
+        /// Lasts the name.
+        /// </summary>
+        /// <param name="lastName">The last name.</param>
+        /// <returns>It returns true if pattern matches.</returns>
         public bool LastName(string lastName)
         {
             var pattern = "^[a-zA-Z]+$";
@@ -31,6 +48,11 @@ namespace XUnitTesting
             return false;
         }
 
+        /// <summary>
+        /// Emails the specified email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns>It returns true if pattern matches.</returns>
         public bool Email(string email)
         {
             var pattern = "^([a-z0-9_.]+)@(gmail|outlook|yahoo).(com|co.in|gov|edu)$";
@@ -43,12 +65,33 @@ namespace XUnitTesting
             return false;
         }
 
+        /// <summary>
+        /// Passwords the specified password.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns>It returns true if pattern matches.</returns>
         public bool Password(string password)
         {
-            //var pattern = "^([a-zA-Z0-9._@]+){8,}$";
-            var passPattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*!])(?=.{8,20})";
-            Regex regex = new Regex(passPattern);
+            var pattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*!])(?=.{8,20})";
+            Regex regex = new Regex(pattern);
             if (regex.IsMatch(password))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Mobiles the specified mobile.
+        /// </summary>
+        /// <param name="mobile">The mobile.</param>
+        /// <returns>It returns true if pattern matches.</returns>
+        public bool Mobile(string mobile)
+        {
+            var pattern = "^[0-9]{10}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(mobile))
             {
                 return true;
             }

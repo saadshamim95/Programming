9e188546403aff6bb50d639723d0b9843dc6f355
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-19 10:51:34 +0530

    [Saad] Refactor. Adding test case for firstname

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace XUnitTesting
 {
     class UserRegistration
     {
+        public bool FirstName(string firstName)
+        {
+            var pattern = "^[a-zA-Z]+$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(firstName))
+            {
+                return true;
+            }
 
+            return false;
+        }
     }
 }
